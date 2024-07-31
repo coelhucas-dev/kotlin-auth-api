af65f3f86de58e2b9519ef76fb01148260651f8f
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-07-31 00:10:17 -0300

    Init repo

---
+++ src/main/kotlin/com/lucascoelho/kotlinauthapi/KotlinAuthApiApplication.kt
@@ -0,0 +1,11 @@
+package com.lucascoelho.kotlinauthapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAuthApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinAuthApiApplication>(*args)
+}
