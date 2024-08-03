92aa543ba82fe264ddcde581d5e69f2e127c1412
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-08-02 22:27:51 -0300

    feat(UserProfileRepository): create findByUserLogin

--- src/main/kotlin/com/lucascoelho/kotlinauthapi/repositories/UserProfileRepository.kt
+++ src/main/kotlin/com/lucascoelho/kotlinauthapi/repositories/UserProfileRepository.kt
@@ -1,9 +1,14 @@
 package com.lucascoelho.kotlinauthapi.repositories
 
-import com.lucascoelho.kotlinauthapi.domain.user.UserProfile
+import com.lucascoelho.kotlinauthapi.domain.user_profile.UserProfile
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface UserProfileRepository: JpaRepository<UserProfile, UUID>
+interface UserProfileRepository: JpaRepository<UserProfile, UUID> {
+    @Query("SELECT up FROM UserProfile up WHERE up.user.login = :login")
+    fun findByUserLogin(@Param("login") login: String): UserProfile?
+}
