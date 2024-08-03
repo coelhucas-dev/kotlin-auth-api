b88b19badb268dd8a1ab7163b1f4590ad8f1a3de
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-08-02 22:28:50 -0300

    feat(service): create user service

---
+++ src/main/kotlin/com/lucascoelho/kotlinauthapi/services/UsersService.kt
@@ -0,0 +1,26 @@
+package com.lucascoelho.kotlinauthapi.services
+
+import com.lucascoelho.kotlinauthapi.domain.user.Users
+import com.lucascoelho.kotlinauthapi.domain.user_profile.UserProfile
+import com.lucascoelho.kotlinauthapi.repositories.UserProfileRepository
+import com.lucascoelho.kotlinauthapi.repositories.UsersRepository
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class UsersService(
+    private val usersRepository: UsersRepository,
+    private val userProfileRepository: UserProfileRepository
+): UserDetailsService {
+    override fun loadUserByUsername(username: String): Users {
+        val user: Users = usersRepository.findByLogin(username)
+            ?: throw UsernameNotFoundException("User not found with username: $username")
+
+        return user
+    }
+
+   fun findUserProfileByUsername(username: String): UserProfile? {
+       return this.userProfileRepository.findByUserLogin(username)
+   }
+}
